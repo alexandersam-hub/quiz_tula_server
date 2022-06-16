57262ba3281c56402942cb741b4511d63ec16666
stan1i <alexandersamedov@gmail.com>
2022-06-16 15:47:56 +0300

    add request v3

--- services/TelegramSendService.js
+++ services/TelegramSendService.js
@@ -6,7 +6,7 @@ class TelegramSendService{
             console.log(data)
             request.post(
                 'http://127.0.0.1:8005/api/message/send',
-                {data},
+                {data:{data}},
                 function (error, response, body) {
                     if (!error && response.statusCode == 200) {
                         console.log(body);
